d70c7741ec4f7ef78e5603c281c11faa791fea84
rosa1-maker <teresawanjiku2000@gmail.com>
2020-01-26 22:47:03 +0300

    created a function where the new user's accounts will be saved

--- working.py
+++ working.py
@@ -12,8 +12,15 @@ def new_users(name_first, name_two, email_adress, user_name, pass_word):
     It creates a new user
     """
     new_user = UserData(name_first, name_two, email_adress, user_name, pass_word)
-    
+
     return new_user
+
+def save_accounts(account)    :
+    """
+    It saves the new user's account
+    """
+    account.save_account()
+    
 
 
 
