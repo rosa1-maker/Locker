3179808c6491a9789984ec518cc6bbb355b9d11c
rosa1-maker <teresawanjiku2000@gmail.com>
2020-01-26 22:44:37 +0300

    created a function to create a new user in working.py

--- working.py
+++ working.py
@@ -6,7 +6,14 @@ from required_data import RequiredData
 
 
 
+def new_users(name_first, name_two, email_adress, user_name, pass_word):
 
+    """
+    It creates a new user
+    """
+    new_user = UserData(name_first, name_two, email_adress, user_name, pass_word)
+    
+    return new_user
 
 
 
