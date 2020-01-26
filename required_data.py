ca9efc09092bae12e32711acb988c1f4eed751f1
rosa1-maker <teresawanjiku2000@gmail.com>
2020-01-26 09:07:38 +0300

    created a new class in user_data.py where users will be able to create an account

---
+++ required_data.py
@@ -0,0 +1,30 @@
+# class RequiredData:
+
+#    """
+#    create new data
+#    """
+
+#    required = []
+
+#    def _init_(self, platform, username, password):
+
+#        self.platform = platform
+#        self.username = username
+#        self.password = password
+
+#    def save_required(self):
+
+#        """
+#        save users objects to the users list
+#        """
+
+#        RequiredData.required.append(self)
+
+#     @classmethod
+
+#     def display_required(cls):
+
+#         """
+#         display the required
+#         """
+#         return cls.required   
---
+++ user_data.py
@@ -0,0 +1,39 @@
+class UserData:
+
+    # """
+    # class to create new user acounts
+    # """
+
+    # create_acount =[]
+
+    # def __init__(self, firstName, lastName, email, username, password):
+
+    #     """
+    #     initializes the class
+    #     """
+
+    #     self.fistName = firstName
+    #     self.lastName = lastName
+    #     self.email = email
+    #     self.username = username
+    #     self.password = password
+
+    # def save_account(self):
+
+    #     """
+    #     saves the new user to create_account list
+    #     """
+
+    #     UserData.create_acount.append(self)
+
+    #  @classmethod()
+    #  def user_login(cls, used_name, used_password):
+    #      """
+    #      checks whether user exist
+    #      """
+    #      for user in UserData.create_acount:
+    #          if user.username == used_name and user.password == used_password
+    #          return user
+    #      return False    
+
+
